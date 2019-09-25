af46979e671b322f0f0a49637a56aad058d42494
Lucas Bueno <lucas-50p>
2019-09-25 11:06:28 -0300

    Endpoint_Clintes_{id} disposivel

--- src/main/java/com/lucas/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucas/cursomc/services/CategoriaService.java
@@ -11,14 +11,13 @@ import com.lucas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-		
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
 }
